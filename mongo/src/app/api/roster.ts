ff110a2cbefdbc05cee08c32cb33f07f4805bc01
bgoff1 <35714383+bgoff1@users.noreply.github.com>
2018-03-19 13:45:18 -0500

    Added structure for roster functions

---
+++ mongo/src/app/api/roster.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+import { User, UserData   } from "../../models/user";
+import { Class, ClassData } from "../../models/class";
+
+export function GetAllStudentsInClass(req: Request, res: Response, next: NextFunction)
+{
+
+}
+
+export function addStudentToClass(req: Request, res: Response, next: NextFunction)
+{
+
+}
+
+export function deleteStudentFromClass(req: Request, res: Response, next: NextFunction)
+{
+
+}
--- mongo/src/app/api/routes.ts
+++ mongo/src/app/api/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import * as bodyParser from "body-parser";
 import * as users from "./users";
 import * as classes from "./classes";
+import * as roster from "./roster";
 
 export const router = Router();
 
@@ -23,3 +24,8 @@ router.get("/classes/:classid", classes.getOneClass);
 router.post("/classes/", classes.addClass);
 router.put("/classes/:classid", classes.updateClass);
 router.delete("/classes/:classid", classes.deleteClass);
+
+// May need work to deal with multiple params not sure
+router.get("/rosters/:classid", roster.GetAllStudentsInClass);
+router.put("/rosters/:classid/:userid", roster.addStudentToClass);
+router.delete("/rosters/:classid/:userid", roster.deleteStudentFromClass);
